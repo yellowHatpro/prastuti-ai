86e4e69d51cf3812c1772482fd95bb7788a6e2f5
yellowhatpro <yellowhatpro3119@gmail.com>
2024-04-18 12:24:54 +0530

    some css stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import SubmitPrompt from "@/components/ui/submit-prompt";
 
 export default function Home() {
   return (
-      <main className="flex min-h-screen h-screen flex-col items-center justify-between p-24 text-black">
+      <main className="flex min-h-screen h-screen w-full flex-col items-center justify-between p-24 text-[#1e1e2e]">
           <h1>We have an AI that creates PPT for you</h1>
           <h1 className="md:text-7xl text-3xl lg:text-6xl font-bold text-center relative z-20">
               Prastuti.ai
